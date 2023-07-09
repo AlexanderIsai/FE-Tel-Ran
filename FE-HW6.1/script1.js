09ad5aa754b24043283df42c4b6af604bff3e8eb
AlexanderIsai <74790416+AlexanderIsai@users.noreply.github.com>
2023-07-09 23:24:03 +0300

    Update script1.js

--- FE-HW6.1/script1.js
+++ FE-HW6.1/script1.js
@@ -1,10 +1,15 @@
 alert('Задача 1. Нажмите "ок" для продолжения')
 let productCost = +prompt("Расчет скидок и наценок. Введите стоимость товара");
 let bodyElem = document.querySelector('body');
+let h1Elem = document.querySelector('h1');
+let h21 = document.querySelector('.h21');
 
 bodyElem.classList.add('basis');
 
 let answer1 = document.querySelector('.task1');
+h1Elem.style.display = "block";
+h21.classList.remove("h21")
+
 
 answer1.innerHTML = `
 Стоимость товара со скидкой 10% - ${(productCost * 0.9).toFixed(2)}
@@ -19,8 +24,8 @@ setTimeout(function () {
     alert('Спасибо за участие переходим к задаче 2. Нажмите "ок" для продолжения');
     let productCost2 = +prompt("Узнаем, положена ли Вам скидка. Введите стоимость товара");
     let answer2 = document.querySelector(".task2");
-    let h21 = document.querySelector('.h21');
-    h21.classList.remove('h21')
+    let h22 = document.querySelector('.h22');
+    h22.classList.remove('h22')
     if (productCost2 >= 100) {
         answer2.textContent = `Т.к. стоимость товара больше или равна 100, мы сделали Вам скидку. Окончательная цена - ${(productCost2 * 0.95).toFixed(2)}`
     } else answer2.textContent = `Стоимость товара - ${productCost2}`;
@@ -30,8 +35,8 @@ setTimeout(function () {
     alert('Спасибо за участие переходим к задаче 3. Нажмите "ок" для продолжения');
     let num = +prompt("Сравнение с 0. Введите число");
     let answer3 = document.querySelector(".task3");
-    let h22 = document.querySelector('.h22');
-    h22.classList.remove('h22')
+    let h23 = document.querySelector('.h23');
+    h23.classList.remove('h23')
     if (num < 0) {
         answer3.textContent = `Ваше число ${num} меньше 0`
     } else if (num > 0) {
@@ -43,8 +48,8 @@ setTimeout(function () {
     alert('Задача 3 завершена. Результаты в консоли. Нажмите "ок" для перехода к задаче 4');
     let num2 = +prompt("Определение последней цифры числа. Введите число");
     let answer4 = document.querySelector(".task4");
-    let h23 = document.querySelector('.h23');
-    h23.classList.remove('h23')
+    let h24 = document.querySelector('.h24');
+    h24.classList.remove('h24')
     answer4.textContent = `Последняя цифра вашего числа = ${num2 % 10}`;
 }, 500)
 
